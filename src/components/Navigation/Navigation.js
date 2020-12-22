3110fe516836eae76a1f074ad8a73bb34c547667
Justin Suderman <jsuderman1@gmail.com>
2020-12-21 21:00:00 -0800

    added buttons to navi bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -16,6 +16,7 @@ function Navigation() {
                 <IconButton>
                     <LinkedInIcon
                     fontSize="large"
+                    
                     />
                 </IconButton>
                 
