62dedc354b6a7d7d482bbbed7de2694e1414e749
Justin Suderman <jsuderman1@gmail.com>
2020-12-26 09:15:52 -0800

    change nav bar to be responsive to show icon when page is smaller

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,40 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Navigation.css";
 import { IconButton } from '@material-ui/core';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import EmailIcon from '@material-ui/icons/Email';
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 
 function Navigation() {
+
+        const [showLinks, setShowLinks] = useState(false);
+
     return (
         <div className="navigation">
-            <div className="navigation__header">
-                <p> Justin Suderman</p>
-            </div> 
-            <div className="navigation__socials">
-                <IconButton>
-                    <LinkedInIcon
-                    fontSize="large"
-                    
-                    />
-                </IconButton>
-                
-                <IconButton>
-                    <GitHubIcon 
-                    fontSize="large"
-                    />
-                </IconButton>
-
-                <IconButton>
-                    <EmailIcon 
-                    fontSize="large"
-                    />
-                </IconButton>
-                
-                
+            <div className="navigation__left">
+                <div className="navigation__links">
+                    <a href="/">About Me</a>
+                    <a href="/">Skills</a>
+                    <a href="/">Projects</a>
+                    <a href="/">Contact Me</a>
+                    <IconButton> <MenuIcon /> </IconButton>
+                </div>
             </div>
-            
+
+            <div className="navigation__right">
+                <div className="navigation__socials">
+                    <IconButton>
+                        <LinkedInIcon
+                            fontSize="large"
+
+                        />
+                    </IconButton>
+
+                    <IconButton>
+                        <GitHubIcon
+                            fontSize="large"
+                        />
+                    </IconButton>
+
+                    <IconButton>
+                        <EmailIcon
+                            fontSize="large"
+                        />
+                    </IconButton>
+
+
+                </div>
+            </div>
+
+
+
+
         </div>
     )
 }
