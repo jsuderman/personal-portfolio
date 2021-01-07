addf43e103c72327a31bae8a5e3bd554109308ae
Justin Suderman <jsuderman1@gmail.com>
2021-01-07 11:57:36 -0800

    added basic skills section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Navigation from "./components/Navigation/Navigation";
 import Header from "./components/Header/Header";
-import About from "./components/About/About"
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
      {/* about me */}
      <About />
      {/* skills */}
+     <Skills />
      {/* projects */}
      {/* contact me */}
      {/* footer */}
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Skills.css";
+
+function Skills() {
+    return (
+        <div className="skills">
+            <div>
+                <h2>skills section</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
